8d157e1dfdb19743d1b55fd631e37f25aa6ed265
Delan Azabani <delan@azabani.com>
2014-06-10 00:26:01 +0800

    performance: render character with colour and blur in a single pass

--- js/character.js
+++ js/character.js
@@ -11,7 +11,6 @@ global.Character = function(name, filename, width, height) {
 };
 
 global.Character.resize = function(width, height) {
-	SCRATCH_BLUR_DATA = SCRATCH_CONTEXT.createImageData(width, height);
 	CHARACTERS.forEach(function(character) {
 		character.cache(width, height);
 	});
@@ -22,70 +21,6 @@ global.Character.shuffle = function() {
 	do global.CURRENT_CHARACTER = global.CHARACTERS[
 		Math.floor(Math.random() * global.CHARACTERS.length)
 	]; while (global.CURRENT_CHARACTER == old);
-};
-
-global.Character.blur_x = function(input_data, output_data, intensity) {
-	var w = input_data.width;
-	var h = input_data.height;
-	var p = input_data.data;
-	var q = output_data.data;
-	var n = intensity;
-	for (var y = 0; y < h; y++) {
-		for (var x = 0; x < w; x++) {
-			var i = 4 * (input_data.width * y + x);
-			var r = 0;
-			var g = 0;
-			var b = 0;
-			var a = 0;
-			var missed = 0;
-			for (var j = -n; j <= n; j++) {
-				if (x + j < 0 || x + j >= w) {
-					missed++;
-				} else {
-					r += p[i + 0 + 4 * j];
-					g += p[i + 1 + 4 * j];
-					b += p[i + 2 + 4 * j];
-					a += p[i + 3 + 4 * j];
-				}
-			}
-			q[i + 0] = r / (2 * n + 1 - missed);
-			q[i + 1] = g / (2 * n + 1 - missed);
-			q[i + 2] = b / (2 * n + 1 - missed);
-			q[i + 3] = a / (2 * n + 1 - missed);
-		}
-	}
-};
-
-global.Character.blur_y = function(input_data, output_data, intensity) {
-	var w = input_data.width;
-	var h = input_data.height;
-	var p = input_data.data;
-	var q = output_data.data;
-	var n = intensity;
-	for (var y = 0; y < h; y++) {
-		for (var x = 0; x < w; x++) {
-			var i = 4 * (input_data.width * y + x);
-			var r = 0;
-			var g = 0;
-			var b = 0;
-			var a = 0;
-			var missed = 0;
-			for (var j = -n; j <= n; j++) {
-				if (y + j < 0 || y + j >= h) {
-					missed++;
-				} else {
-					r += p[i + 0 + 4 * w * j];
-					g += p[i + 1 + 4 * w * j];
-					b += p[i + 2 + 4 * w * j];
-					a += p[i + 3 + 4 * w * j];
-				}
-			}
-			q[i + 0] = r / (2 * n + 1 - missed);
-			q[i + 1] = g / (2 * n + 1 - missed);
-			q[i + 2] = b / (2 * n + 1 - missed);
-			q[i + 3] = a / (2 * n + 1 - missed);
-		}
-	}
 };
 
 global.Character.prototype.cache = function(output_width, output_height) {
@@ -119,48 +54,55 @@ global.Character.prototype.cache = function(output_width, output_height) {
 global.Character.prototype.draw = function(timestamp, context, scheme) {
 	var w = context.canvas.width;
 	var h = context.canvas.height;
-	var x = this.image_data;
-	var y = context.getImageData(0, 0, w, h);
-	var z = SCRATCH_BLUR_DATA;
+	var p = this.image_data;
+	var q = context.getImageData(0, 0, w, h);
 	var fr = scheme.fr;
 	var fg = scheme.fg;
 	var fb = scheme.fb;
 	var br = scheme.br;
 	var bg = scheme.bg;
 	var bb = scheme.bb;
-	for (var i = 0; i < w * h; i++) {
-		if (x.data[4 * i + 3] > 127) {
-			y.data[4 * i + 0] = fr;
-			y.data[4 * i + 1] = fg;
-			y.data[4 * i + 2] = fb;
-			y.data[4 * i + 3] = 255;
-		} else {
-			y.data[4 * i + 0] = br;
-			y.data[4 * i + 1] = bg;
-			y.data[4 * i + 2] = bb;
-			y.data[4 * i + 3] = 255;
+	var increment = CURRENT_CHARACTER_BLUR_DIRECTION ? w : 1;
+	var n = 0;
+	var animation_fraction = (
+		timestamp - CURRENT_CHARACTER_BLUR_TIMESTAMP
+	) / 200;
+	if (0 < animation_fraction && animation_fraction < 1)
+		n = ((w / 20) * (1 - animation_fraction)) | 0;
+	for (var y = 0; y < h; y++) {
+		for (var x = 0; x < w; x++) {
+			var i = 4 * (w * y + x);
+			var r = 0;
+			var g = 0;
+			var b = 0;
+			var missed = 0;
+			for (var j = -n; j <= n; j++) {
+				if (y + j < 0 || y + j >= h) {
+					missed++;
+				} else if (
+					p.data[i + 3 + 4 * increment * j] >
+					127
+				) {
+					r += fr;
+					g += fg;
+					b += fb;
+				} else {
+					r += br;
+					g += bg;
+					b += bb;
+				}
+			}
+			q.data[i + 0] = r / (2 * n + 1 - missed);
+			q.data[i + 1] = g / (2 * n + 1 - missed);
+			q.data[i + 2] = b / (2 * n + 1 - missed);
+			q.data[i + 3] = 255;
 		}
 	}
-	if (CURRENT_CHARACTER_BLUR_TIMESTAMP) {
-		var animation_fraction = (
-			timestamp - CURRENT_CHARACTER_BLUR_TIMESTAMP
-		) / 160;
-		if (0 < animation_fraction && animation_fraction < 1) {
-			var p = w / 10;
-			var n = (p - animation_fraction * p) | 0;
-			if (global.CURRENT_CHARACTER_BLUR_DIRECTION)
-				Character.blur_y(y, z, n);
-			else
-				Character.blur_x(y, z, n);
-			y = z;
-		}
-	}
-	context.putImageData(y, 0, 0);
+	context.putImageData(q, 0, 0);
 };
 
 var SCRATCH_CANVAS = document.createElement('canvas');
 var SCRATCH_CONTEXT = SCRATCH_CANVAS.getContext('2d');
-var SCRATCH_BLUR_DATA;
 
 global.CURRENT_CHARACTER = null;
 global.CURRENT_CHARACTER_BLUR_TIMESTAMP = null;
